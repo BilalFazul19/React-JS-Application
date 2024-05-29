6b99bd8637acff175f3b1dbf62169fc6dee05e30
Bilal_Fazul <162340544+BilalFazul19@users.noreply.github.com>
2024-05-28 18:44:08 -1200

    Add files via upload

---
+++ client/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+        <div className="overlay">
+        <h1>Welcome to Zam Zam Eletronics</h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
